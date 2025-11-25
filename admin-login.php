11af1f06a34b8d4d85f1738f62d970354d2b5d82
Dulon Akhter Sathee <0562310005101018@neub.edu.bd>
2025-11-25 20:54:50 +0600

    5th submission

--- admin-login.php
+++ admin-login.php
@@ -4,18 +4,18 @@ error_reporting(0);
 include('includes/config.php');
 
 if($_SESSION['alogin']!=''){
-$_SESSION['alogin']='';
+    $_SESSION['alogin']='';
 }
 if(isset($_POST['login'])){
     $uname = $_POST['username'];
     $password = md5($_POST['password']);
-    
+
     $sql ="SELECT UserName,Password FROM admin WHERE UserName=:uname AND Password=:password";
     $query= $dbh -> prepare($sql);
     $query-> bindParam(':uname', $uname, PDO::PARAM_STR);
     $query-> bindParam(':password', $password, PDO::PARAM_STR);
     $query-> execute();
-    
+
     if($query->rowCount() > 0){
         $_SESSION['alogin']=$_POST['username'];
         header("location: dashboard.php");
@@ -30,9 +30,59 @@ if(isset($_POST['login'])){
 <head>
 <meta charset="utf-8">
 <title>Admin Login</title>
-<link rel="stylesheet" href="asset/css/style.css">
 
+<style>
+.title {
+    text-align: center;
+    margin-top: 40px;
+    font-size: 28px;
+    font-weight: bold;
+}
 
+.login-box {
+    width: 350px;
+    background: #ffffff;
+    padding: 25px;
+    margin: 60px auto;
+    box-shadow: 0px 0px 10px rgba(0,0,0,0.2);
+    border-radius: 8px;
+    text-align: center;
+}
+
+.login-box h2 {
+    margin-bottom: 20px;
+    font-size: 22px;
+}
+
+label {
+    float: left;
+    font-size: 14px;
+    margin: 8px 0 3px 3px;
+}
+
+input {
+    width: 100%;
+    padding: 10px;
+    margin-bottom: 12px;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+}
+
+button {
+    width: 100%;
+    padding: 10px;
+    background: #28a745;
+    border: none;
+    color: #fff;
+    font-size: 17px;
+    border-radius: 5px;
+    cursor: pointer;
+}
+
+button:hover {
+    background: #218838;
+}
+</style>
 
 </head>
 
@@ -52,8 +102,6 @@ if(isset($_POST['login'])){
 
         <button type="submit" name="login">Sign In</button>
     </form>
-
-    
 </div>
 
 </body>
